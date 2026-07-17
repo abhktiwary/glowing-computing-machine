b96d073dba53f98e2314558aabf62cbcb69dbac2
abhishektiwari003 <abhishektiwari@gofynd.com>
2026-07-17 12:49:15 +0530

    ds

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = 8080;
 const server = http.createServer((req, res) => {
   if (req.method === "GET" && req.url === "/") {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Push code ing builder -> Test 16 July updateing");
+    res.end("Push codeuilder -> Test 16 July updateing");
   } else {
     res.writeHead(404, { "Content-Type": "text/plain" });
     res.end("Not Found");
